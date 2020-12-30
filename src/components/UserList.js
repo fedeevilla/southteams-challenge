191362b9ba1406788bfe1ecd5d51b58ab238e432
Federico Villa <fedevilla88@gmail.com>
2020-12-30 12:04:34 -0300

    Added framer-motion

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -6,6 +6,27 @@ import UserCard from "./UserCard";
 import * as R from "ramda";
 import { removeAccents } from "../utils/strings";
 import Select from "./Select";
+import { motion } from "framer-motion";
+
+const container = {
+  hidden: { opacity: 1, scale: 0 },
+  visible: {
+    opacity: 1,
+    scale: 1,
+    transition: {
+      delayChildren: 0.3,
+      staggerChildren: 0.2,
+    },
+  },
+};
+
+const item = {
+  hidden: { y: 20, opacity: 0 },
+  visible: {
+    y: 0,
+    opacity: 1,
+  },
+};
 
 const Wrapper = styled.div`
   display: flex;
@@ -74,11 +95,15 @@ const UserList = () => {
         </Select>
       </Wrapper>
       {userList.length > 0 ? (
-        <Grid>
-          {userList.map((user) => (
-            <UserCard key={user.login.uuid} user={user} />
-          ))}
-        </Grid>
+        <motion.div variants={container} initial="hidden" animate="visible">
+          <Grid>
+            {userList.map((user) => (
+              <motion.div variants={item}>
+                <UserCard key={user.login.uuid} user={user} />
+              </motion.div>
+            ))}
+          </Grid>
+        </motion.div>
       ) : (
         <NoResults>No Results</NoResults>
       )}
